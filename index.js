3bafa5b4be91e08745281260288ec96d1fdc0e9f
rakibhassannayem <rakibhassannayem@gmail.com>
2025-08-27 00:01:08 +0600

    copy to paste done

--- index.js
+++ index.js
@@ -8,12 +8,23 @@ let coinCount = 100;
 // delegation
 
 getElement("services").addEventListener("click", function (e) {
+  
+  const name = e.target.parentNode.parentNode.children[1].children[0].innerHTML;
+  const type = e.target.parentNode.parentNode.children[1].children[1].innerText;
+  const number =
+    e.target.parentNode.parentNode.children[1].children[2].innerText;
+
   if (e.target.id.includes("heart-btn")) {
     heartCount++;
     getElement("heart-count").innerText = heartCount;
   } else if (e.target.id.includes("copy-btn")) {
     copyCount++;
     getElement("copy-count").innerText = copyCount;
+
+    // Copy to clipboard
+    navigator.clipboard.writeText(number);
+
+    alert("Coppied!");
   } else if (e.target.id.includes("call-btn")) {
     if (coinCount < 20) {
       alert("❌ আপনার পর্যাপ্ত কয়েন নেই! কল করতে কমপক্ষে ২০ কয়েন লাগবে।");
@@ -21,12 +32,6 @@ getElement("services").addEventListener("click", function (e) {
       coinCount -= 20;
       getElement("coin-count").innerText = coinCount;
 
-      const name =
-        e.target.parentNode.parentNode.children[1].children[0].innerHTML;
-      const type =
-        e.target.parentNode.parentNode.children[1].children[1].innerText;
-      const number =
-        e.target.parentNode.parentNode.children[1].children[2].innerText;
       alert(`📞 Calling ${type} ${number}...`);
       const time = new Date().toLocaleTimeString();
 
@@ -47,6 +52,6 @@ getElement("services").addEventListener("click", function (e) {
   }
 });
 
-getElement("btn-clear").addEventListener("click",function(){
-  getElement("history-container").innerHTML = ""
-})
+getElement("btn-clear").addEventListener("click", function () {
+  getElement("history-container").innerHTML = "";
+});
