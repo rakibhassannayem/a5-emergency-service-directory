aa65013dc27ac635aab07a270f2512964fc7f54b
rakibhassannayem <rakibhassannayem@gmail.com>
2025-08-26 21:44:12 +0600

    copy count done

--- index.js
+++ index.js
@@ -8,8 +8,12 @@ let coinCount = 100;
 // delegation
 
 getElement("services").addEventListener("click", function (e) {
+  // heart count
   if (e.target.id.includes("heart-btn")) {
     heartCount++;
     getElement("heart-count").innerText = heartCount;
+  } else if (e.target.id.includes("copy-btn")) {
+    copyCount++;
+    getElement("copy-count").innerText = copyCount;
   }
 });
