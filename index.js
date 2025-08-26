4f75c18d3e892cb3aca3b9d099854675b5db21c1
rakibhassannayem <rakibhassannayem@gmail.com>
2025-08-26 22:56:20 +0600

    call button alert done

--- index.js
+++ index.js
@@ -15,5 +15,18 @@ getElement("services").addEventListener("click", function (e) {
   } else if (e.target.id.includes("copy-btn")) {
     copyCount++;
     getElement("copy-count").innerText = copyCount;
+  } else if (e.target.id.includes("call-btn")) {
+    if (coinCount < 20) {
+      alert("❌ আপনার পর্যাপ্ত কয়েন নেই! কল করতে কমপক্ষে ২০ কয়েন লাগবে।");
+    } else {
+      coinCount -= 20;
+      getElement("coin-count").innerText = coinCount;
+
+      const name =
+        e.target.parentNode.parentNode.children[1].children[1].innerText;
+      const number =
+        e.target.parentNode.parentNode.children[1].children[2].innerText;
+      alert(`📞 Calling ${name} ${number}...`);
+    }
   }
 });
