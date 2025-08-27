92b08f6a6c44bfba54ea2f21251ecac33c472771
rakibhassannayem <rakibhassannayem@gmail.com>
2025-08-27 13:34:48 +0600

    fix heart count issue

--- index.js
+++ index.js
@@ -6,6 +6,16 @@ let copyCount = 0;
 let coinCount = 100;
 
 
+// heart button functionality
+const heartBtns = document.querySelectorAll("#heart-btn");
+for (const btn of heartBtns) {
+  btn.addEventListener("click", function () {   
+    heartCount++;
+    getElement("heart-count").innerText = heartCount;
+
+  });
+}
+
 // copy button functionality
 const copyBtns = document.querySelectorAll("#copy-btn");
 for (const btn of copyBtns) {
